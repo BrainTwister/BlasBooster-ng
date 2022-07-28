ce2ec378e499beddae972b9bf776b1a0cd5efe68
Bernd Doser <bernd.doser@braintwister.eu>
2022-07-28 12:16:21 +0000

    add conan and catch2

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,8 +1,11 @@
+#include <catch2/catch_test_macros.hpp>
 #include "DenseMatrix.h"
 
-int main()
+TEST_CASE("DenseMatrix", "default constructor")
 {
     using namespace BlasBooster;
 
     Matrix<Dense, double> A;
+
+    REQUIRE( A.size() == 0 );
 }
